334c9380253c0f6f85d8a347d5c4579aabeafc8b
Mayank Kumar <mayank.k9802@gmail.com>
2022-11-28 19:06:53 +0530

    q7

--- Proj.py
+++ Proj.py
@@ -4,6 +4,7 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+import os
 
 listner=sr.Recognizer()
 engine=pyttsx3.init()
@@ -30,29 +31,106 @@ def take_command():
       pass
     return command
 
-def sendW
+def sendWhatsappMessage(phone_number, message):
+    currenthour = datetime.datetime.now().hour
+    currentminute = datetime.datetime.now().minute
+    pywhatkit.sendwhatmsg(phone_number, message, currenthour, currentminute+1,20,True,5)
+
+def twoParagraphs(text):
+    return text.split(".")[0] + "." + text.split(".")[1] + "."
+
+def openNotepad():
+    os.system("notepad.exe")
+
+def openPaint():
+    os.system("mspaint.exe")
+
+def openCalculator():
+    os.system("calc.exe")
+
+def openWordpad():
+    os.system("write.exe")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def run_alexa():
   command=take_command()
   print(command)
+
   if("play" in command):
     song=command.replace("play","")
     talk("playing"+song)
     pywhatkit.playonyt(song)
+  
   elif("time" in command):
     time=datetime.datetime.now().strftime('%I:%M %p')
     talk("Current time is"+time)
+  
   elif("who the heck is" in command):
     person=command.replace("who the heck is","")
     info=wikipedia.summary(person)
-    print(info)
-    talk(info)
+    print(twoParagraphs(info))
+    talk(twoParagraphs(info))
+  
   elif("date" in command):
     talk("Sorry I have a headache")
+  
   elif("are you single" in command):
     talk("I am in a relationship with my wifi")
+  
   elif("joke" in command):
     talk(pyjokes.get_joke())
+  
+  # elif("wordpad" in command):
+
+  elif("wordpad" in command):
+    talk("Opening wordpad")
+    openWordpad()
+
+  elif("paint" in command):
+    talk("Opening Paint")
+    openPaint
+
+  elif("calculator" in command):
+    talk("Opening calculator")
+    openCalculator()
+
+  elif("notepad" in command):
+    talk("Opening Notepad")
+    openNotepad()
+
+  elif("whatsapp" in command):
+    talk("Who do you want to send a message")
+    contact=take_command()
+    
+    if("contact1" in contact):
+      talk("What message do you want to send")
+      message=take_command()
+      sendWhatsappMessage(contact1,message)
+    
+    elif("contact2" in contact):
+      talk("What message do you want to send")
+      message=take_command()
+      sendWhatsappMessage(contact2,message)
+    
+    else:
+      talk("Contact not recognised")
+  
   else:
     talk("please say the command again")
 
